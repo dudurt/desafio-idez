6b57750c7ab7e32c4e5832373c89b11c99e70dfa
Eduardo Martins Padilha <dudurtmp@gmail.com>
2023-06-29 23:13:45 -0300

    feat/create-web-routes

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('city')->group(function () {
     Route::get('list/{stateAcronym}', [CityController::class, 'listByState']);
 });
-
-Route::any('/', function () {
-    return "Start of idez project...";
-});
